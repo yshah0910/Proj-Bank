ca5fab79776f0138841e4f42dd5376cd80bfe2f8
yshah0910 <145277056+yshah0910@users.noreply.github.com>
2024-12-09 12:22:30 -0500

    Update urls.py

--- bank/bank/bankk/urls.py
+++ bank/bank/bankk/urls.py
@@ -2,8 +2,10 @@ from django.urls import path
 from bank.bankk import views
 
 urlpatterns = [
-    path('', views.login_view, name='login'),
+    path('', views.login_view, name='login_view'),
     path('customer/<int:ssn>/', views.customer_dashboard, name='customer_dashboard'),
     path('employee/<int:ssn>/', views.employee_dashboard, name='employee_dashboard'),
     path('branch/<int:branch_id>/', views.branch_dashboard, name='branch_dashboard'),
+    path('signup/', views.signup_view, name='signup_view'),
+
 ]
